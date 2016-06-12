210bbde09225c6f7cb410decaa7993501f5cd554
NaturaAurum <natura>
2016-06-13 00:14:09 +0900

    점프 구현중.

---
+++ Assets/Scripts/Test/TestScript_1.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// 프로토 타입용 스크립트
+/// 여기다 점프에 관련된 모든걸 박는다
+/// </summary>
+public class TestScript_1 : MonoBehaviour
+{
+    private Rigidbody2D playerRig = null;
+
+    private bool isStartGame = false;
+
+    private GameObject text = null;
+
+    // Use this for initialization
+    void Awake()
+    {
+        playerRig = GetComponent<Rigidbody2D>();
+        text = GameObject.Find( "StartText" );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetMouseButtonDown( 0 ) )
+        {
+            text.SetActive( false );
+            if ( !isStartGame )
+            {
+                FirstMove();
+            }
+            isStartGame = true;
+        }
+
+        if ( isStartGame )
+        {
+            PlayerMove();
+        }
+    }
+
+    private void FirstMove()
+    {
+
+    }
+
+    private void PlayerMove()
+    {
+
+    }
+}
