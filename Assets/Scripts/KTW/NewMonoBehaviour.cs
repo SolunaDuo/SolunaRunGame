8e4dbc630bc921067661f60fc2d5e752011ac8e2
NaturaAurum <natura0321@gmail.com>
2016-06-28 00:30:38 +0900

    포물선 실험 중

---
+++ Assets/Scripts/KTW/NewMonoBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewMonoBehaviour : MonoBehaviour
+{
+
+    GameObject player; Vector3 startPos; Vector3 destPos; float timer; float vx; float vy; float vz;  // Use this for initialization 
+    void Start()
+    {
+        startPos = transform.position;
+        destPos = player.transform.position;
+        vx = ( destPos.x - startPos.x ) / 2f;
+        vz = ( destPos.z - startPos.z ) / 2f;
+        vy = ( destPos.y - startPos.y + 2 * 9.8f ) / 2f;
+    }// Update is called once per frame 
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float sx = startPos.x + vx * timer;
+        float sy = startPos.y + vy * timer - 0.5f * 9.8f * timer * timer;
+        float sz = startPos.z + vz * timer;
+        transform.position = new Vector3( sx, sy, sz );
+        transform.Rotate( 1.2f, 0, 0 );
+    }
+}
--- Assets/Scripts/KTW/TestScript_1.cs
+++ Assets/Scripts/KTW/TestScript_1.cs
@@ -14,80 +14,79 @@ public class TestScript_1 : MonoBehaviour
 
     private GameObject text = null;
 
-    public float power = 10f;
+    public float power;
 
-    private Vector2 fixPosition;
+    private Vector2 left;
+    private Vector2 right;
+    private Vector2 target;
+    private Vector2 start;
 
-    public bool isFix;
+    private float xSp = 0.0f;
+    private float ySp = 0.0f;
+
+    private float timer = 0.0f;
+
+    private bool startJump = false;
 
     // Use this for initialization
     void Awake()
     {
         playerRig = GetComponent<Rigidbody2D>();
         text = GameObject.Find( "StartText" );
+
+        left = GameObject.Find( "LeftWall" ).transform.position;
+        right = GameObject.Find( "RightWall" ).transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( !isStartGame )
-        {
-            FirstMove();
-        }
-        PlayerMove();
-        if ( isFix )
-        {
-            transform.position = fixPosition;
-        }
-    }
-
-    private void FirstMove()
-    {
-        if ( Input.GetMouseButtonDown( 0 ) || Input.GetMouseButtonDown( 1 ) )
-        {
-            Jump( ( Vector2.right + Vector2.up ).normalized );
-            transform.Rotate( 0, 0, 90 );
-            isStartGame = true;
-            playerRig.gravityScale = 0f;
-            Utils.Event.Send( "start_game" );
-        }
-    }
-
-    private void PlayerMove()
-    {
         if ( Input.GetMouseButtonDown( 0 ) )
         {
-            Jump( Vector2.left);
+            target = left;
+            JumpInfoInit();
         }
         else if ( Input.GetMouseButtonDown( 1 ) )
         {
-            Jump( Vector2.right );
+            target = right;
+            JumpInfoInit();
+        }
+
+        if ( startJump )
+        {
+            Jump();
         }
     }
 
-    private void Jump( Vector2 dir )
+    private void JumpInfoInit()
     {
-        playerRig.AddForce( dir * power, ForceMode2D.Force );
-        transform.localScale = new Vector3( transform.localScale.x, ( dir.x < 0 ) ? -1 : 1, transform.localScale.z );
-        UnFix();
+        if(!isStartGame)
+        {
+            Utils.Event.Send( "start_game" );
+            isStartGame = true;
+        }
+        playerRig.isKinematic = false;
+        start = transform.position;
+        xSp = ( target.x - start.x ) / 2f;
+        ySp = ( target.y - start.y + 2 * 9.8f ) / 2f;
+        startJump = true;
+    }
+
+    private void Jump()
+    {
+        timer += Time.deltaTime;
+        float x = start.x + xSp * timer;
+        float y = start.y + ySp * timer - 0.5f * 9.8f * timer * timer;
+        transform.position = new Vector2( x, y );
     }
 
     void OnCollisionEnter2D( Collision2D pCollision )
     {
         if ( pCollision.gameObject.name.Contains( "Wall" ) )
         {
-            Fix( transform.position );
+            startJump = false;
+            playerRig.isKinematic = true;
         }
     }
 
-    private void Fix( Vector2 pos )
-    {
-        isFix = true;
-        fixPosition = pos;
-    }
-
-    private void UnFix()
-    {
-        isFix = false;
-    }
 }
