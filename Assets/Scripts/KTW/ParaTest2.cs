8af5ef7bf4cdfcc877598edb22b65437994877df
NaturaAurum <natura0321@gmail.com>
2016-07-24 00:39:52 +0900

    ParaTest 중.

--- Assets/Scripts/KTW/ParaTest2.cs
+++ Assets/Scripts/KTW/ParaTest2.cs
@@ -7,9 +7,13 @@ public class ParaTest2 : MonoBehaviour
 
     private Transform start;
     private Transform target;
+    private Vector3 targetPos;
 
     private Transform leftTarget;
     private Transform rightTarget;
+
+    private Vector3 leftTargetPos;
+    private Vector3 rightTargetPos;
 
     private float journeyTime = 1.0f;
     private float startTime = 0.0f;
@@ -21,8 +25,20 @@ public class ParaTest2 : MonoBehaviour
     {
         startTime = Time.time;
         start = transform;
-        leftTarget = GameObject.Find( "LeftWall" ).transform;
-        rightTarget = GameObject.Find( "RightWall" ).transform;
+        //leftTarget = GameObject.Find( "LeftWall" ).transform;
+        //rightTarget = GameObject.Find( "RightWall" ).transform;
+
+        var middleMap = GameObject.Find( "Map_02" ).transform;
+
+
+        leftTargetPos = middleMap.Find( "LeftWall" ).position;
+        rightTargetPos = middleMap.Find( "RightWall" ).position;
+
+        leftTargetPos = new Vector3( leftTargetPos.x + 0.5f, leftTargetPos.y, leftTargetPos.z );
+        rightTargetPos = new Vector3( rightTargetPos.x - 0.5f, rightTargetPos.y, rightTargetPos.z );
+
+        Debug.Log( leftTargetPos );
+        Debug.Log( rightTargetPos );
     }
 
     // Update is called once per frame
@@ -30,12 +46,14 @@ public class ParaTest2 : MonoBehaviour
     {
         if ( Input.GetMouseButtonDown( 0 ) )
         {
-            target = leftTarget;
+            //target = leftTarget;
+            targetPos = leftTargetPos;
             isJumping = true;
         }
         else if ( Input.GetMouseButtonDown( 1 ) )
         {
-            target = rightTarget;
+            //target = rightTarget;
+            targetPos = rightTargetPos;
             isJumping = true;
         }
 
@@ -47,21 +65,18 @@ public class ParaTest2 : MonoBehaviour
 
     private void Jump()
     {
-        Vector3 center = ( start.position + target.position ) * 0.5f;
+        Vector3 center = ( start.position + targetPos ) * 0.5f;
         center -= Vector3.up;
         Vector3 startRelCenter = start.position - center;
-        Vector3 targetRelCenter = target.position - center;
+        Vector3 targetRelCenter = targetPos - center;
         //float frac = ( Time.time - startTime ) / journeyTime;
         float frac = journeyTime * Time.deltaTime;
         transform.position = Vector3.Slerp( startRelCenter, targetRelCenter, frac );
         transform.position += center;
     }
 
-    void OnCollisionEnter2D( Collision2D pCollision )
-    {
-        Debug.Log( "!" );
-        if ( pCollision.gameObject.name.Contains( "Wall" ) )
-        {
+    void OnTriggerEnter2D(Collider2D pCollider) {
+        if( pCollider.gameObject.name.Contains( "Wall" ) ) {
             isJumping = false;
         }
     }
