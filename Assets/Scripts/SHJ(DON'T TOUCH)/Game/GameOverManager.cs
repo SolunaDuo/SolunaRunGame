dacd22595af1ffe11d7de18af67ff64d311069b0
SHJ <junwe99@gmail.com>
2016-07-24 21:23:21 +0900

    0724)

---
+++ Assets/Scripts/SHJ(DON'T TOUCH)/Game/GameOverManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour {
+    public static GameOverManager instance = null;
+    public Cards[] Cards;
+
+    void Awake()
+    {
+        instance = this;
+    }
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetReulstText(int idx, string szmsg, int restulnum)
+    {
+        if (idx > Cards.Length)
+        {
+            Debug.Log("Cards idx OverFlow!");
+            return;
+        }
+        Cards[idx].tResult.text = szmsg + restulnum;
+    }
+}
---
+++ Assets/Scripts/SHJ(DON'T TOUCH)/Game/HundleTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HundleTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // 장애물과 충동했을 경우 나중에 플레이어에서 따로 처리해야할듯
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag.Equals("Player"))
+        {
+            GameManager.instance.bPlayMode = false;
+        }
+    }
+}
