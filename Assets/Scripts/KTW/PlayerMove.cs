b5ec5ca240d8496988a29663ba72f0de19a657a1
NaturaAurum <natura0321@gmail.com>
2016-08-21 02:41:16 +0900

    점프할때 회전하도록 해보았다. 리소스 회전 시켜둔것 따로 뺴둠. 이단 점프 구현 해야함.

--- Assets/Scripts/KTW/PlayerMove.cs
+++ Assets/Scripts/KTW/PlayerMove.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 
+    public enum JumpDirection {
+        NONE,
+        LEFT,
+        RIGHT,
+    }
+
     private Vector3 startPos;
     private Vector3 endPos;
 
@@ -27,40 +33,51 @@ public class PlayerMove : MonoBehaviour {
     [SerializeField]
     private float targetMinus = 0.0f;
 
-    private Rigidbody2D rig;
+    private PlayerAnim animControl;
+
+    private SpriteRenderer playerRenderer;
+
+    private JumpDirection j_dir = JumpDirection.NONE;
 
     // Use this for initialization
     void Awake() {
         startPos = transform.position;
 
-        targetMinus = transform.localScale.x + 0.5f;
+        targetMinus = transform.localScale.x / 3f + 0.5f;
 
         leftEndPos = GameObject.Find( "Map_02" ).transform.Find( "LeftWall" ).position;
         rightEndPos = GameObject.Find( "Map_02" ).transform.Find( "RightWall" ).position;
 
-        rig = GetComponent<Rigidbody2D>();
-
         leftEndPos.x += targetMinus;
         rightEndPos.x -= targetMinus;
+
+        leftEndPos.y -= 2f;
+        rightEndPos.y -= 2f;
+
+        animControl = GetComponent<PlayerAnim>();
+        playerRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update() {
         if( Input.GetKeyDown( KeyCode.A ) ) {
+            j_dir = JumpDirection.LEFT;
             isJumping = true;
             endPos = leftEndPos;
         }
         else if( Input.GetKeyDown( KeyCode.D ) ) {
+            j_dir = JumpDirection.RIGHT;
             isJumping = true;
             endPos = rightEndPos;
         }
 
         if( isJumping ) {
+            JumpRotation();
             Jumping();
         }
     }
 
-    public void Jumping() {
+    private void Jumping() {
         t += Time.deltaTime * speed;
         if( startCurve ) {
             Vector3 center = ( startPos + endPos ) * 0.5f;
@@ -74,10 +91,20 @@ public class PlayerMove : MonoBehaviour {
         if( t > 1.0f ) {
             t = 0.0f;
             if( startCurve == false ) {
+                playerRenderer.flipY = true;
                 startCurve = true;
             }
             isJumping = false;
+            animControl.Play( PlayerAnim.Animation.Run_2 );
             startPos = transform.position;
         }
+    }
+
+    private void JumpRotation() {
+        animControl.Play( PlayerAnim.Animation.Jump );
+        playerRenderer.flipX = ( j_dir != JumpDirection.RIGHT );
+
+        int sign = ( j_dir == JumpDirection.LEFT ) ? 1 : -1;
+        transform.rotation = Quaternion.Euler( 0, 0, ( 90f * sign ) * t * 5f );
     }
 }
